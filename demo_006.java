1a34be3b722e325a6c86e42b30ee99c5627f9ed5
venkatsai713 <venkatsaiyerragunta456@gmail.com>
2025-01-03 10:15:22 +0530

    Verifying of Username and Password of my name

---
+++ demo_006.java
@@ -0,0 +1,11 @@
+public class demo_006 {
+    public static void main(String[] args) {
+        String username = "sai";
+        String password = "sai@123";
+
+        while ((username == "sai") && (password == "sai@123")) {
+            System.out.println("Hello sai!");
+            break;
+        }
+    }
+}
