0c12c18e0cf76f4a36d3aa88ca9fc8051d86667c
venkatsai713 <venkatsaiyerragunta456@gmail.com>
2025-01-20 10:15:25 +0530

    Printing 10 friends names by using Array

---
+++ demo_021.java
@@ -0,0 +1,11 @@
+public class demo_021 {
+    public static void main(String[] args) {
+        String[] names = {
+            "Sai", "Bhanu", "Babby", "Banny", "Rishi", "Arun", "Sathvik", "Yashwanth", "Vineesh", "Shiva"
+        };
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
